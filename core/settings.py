4ae460fef147ec69028416d3a50b9fd9db7908a3
aditya.aries <102455608+adityaaries23@users.noreply.github.com>
2025-03-23 14:23:45 +0700

    - setup postgres db

--- core/settings.py
+++ core/settings.py
@@ -9,11 +9,13 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv(override=True)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
@@ -78,8 +80,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('PG_NAME','postgres'),
+        'USER': os.environ.get('PG_USER','postgres'),
+        'PASSWORD': os.environ.get('PG_PASSWORD','<PASSWORD>'),
+        'HOST': os.environ.get('PG_HOST','localhost'),
+        'PORT': os.environ.get('PG_PORT','5432'),
     }
 }
 
